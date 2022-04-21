078e4e37eaa51ea04c70b9a3ba3cca2b8b7129db
carolynhammond <caroham@live.unc.edu>
2022-04-21 15:48:34 -0400

    first

---
+++ server.js
@@ -0,0 +1,199 @@
+const express = require('express')
+const app = express()
+
+const db = require('./log.db')
+
+const morgan = require('morgan')
+const errorhandler = require('errorhandler')
+const fs = require('fs')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+const args = require("minimist")(process.argv.slice(2))
+
+args["port"]
+
+const port = args.port || process.env.PORT || 5555
+
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+});
+
+app.use( (req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next()
+
+    })
+
+if (args.log == true) {
+// Create a write stream to append (flags: 'a') to a file
+const WRITESTREAM = fs.createWriteStream('FILE', { flags: 'a' })
+// Set up the access logging middleware
+app.use(morgan('combined', { stream: WRITESTREAM }))
+}
+
+//if (args.debug == true) {
+
+//}
+function coinFlip() {
+    var x = Math.round(Math.random());
+    if (x < 1) {return "heads";} else {return "tails";}
+  
+  }
+
+  function coinFlips(flips) {
+    var arr = [];
+    for (let i = 0; i < flips; i++) {
+      arr[i] = coinFlip();
+    }
+    return arr;
+  }
+
+  function countFlips(array) {
+    let h = 0;
+    let t = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == "heads") {
+        h++;
+      } else {
+        t++;
+      }
+    }
+    return {heads: h, tails: t};
+  }
+
+  function flipACoin(call) {
+    let flip = coinFlip();
+    if (flip == call) {
+      return {"call": call, "flip": flip, "result": "win"}; 
+    } else {
+      return {"call": call, "flip": flip, "result": "lose"}; 
+    }
+  }
+
+  app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
+});
+
+// CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new/user", (req, res, next) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
+    }
+    const stmt = db.prepare('INSERT INTO userinfo (username, password) VALUES (?, ?)')
+    const info = stmt.run(data.user, data.pass)
+    res.status(200).json(info)
+});
+
+// READ a list of users (HTTP method GET) at endpoint /app/users/
+app.get("/app/users", (req, res) => {	
+    try {
+        const stmt = db.prepare('SELECT * FROM userinfo').all()
+        res.status(200).json(stmt)
+    } catch {
+        console.error(e)
+    }
+});
+
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ?').get(req.params.id);
+        res.status(200).json(stmt)
+    } catch (e) {
+        console.error(e)
+    }
+
+});
+
+// UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
+    }
+    const stmt = db.prepare('UPDATE userinfo SET username = COALESCE(?,username), password = COALESCE(?,password) WHERE id = ?')
+    const info = stmt.run(data.user, data.pass, req.params.id)
+    res.status(200).json(info)
+});
+
+// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+    const stmt = db.prepare('DELETE FROM userinfo WHERE id = ?')
+    const info = stmt.run(req.params.id)
+    res.status(200).json(info)
+});
+// Default response for any other request
+app.use(function(req, res){
+	res.json({"message":"Endpoint not found. (404)"});
+    res.status(404);
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Server stopped')
+    })
+})
+
+
+//from a03
+    app.get('/app/flip/', (req, res) => {
+        const flip = coinFlip()
+        res.status(200).json({'flip' : flip})
+    });
+    
+    app.get('/app/flips/:number/', (req, res) => {
+        const flips = coinFlips(req.params.number)
+        const count = countFlips(flips)
+        res.status(200).json({'raw' : flips, 'summary' : count})
+    });
+
+    app.get('/app/flip/call/heads', (req, res) => {
+        res.status(200).json(flipACoin("heads"))
+    });
+
+    app.get('/app/flip/call/tails', (req, res) => {
+        res.status(200).json(flipACoin("tails"))
+    });
+
